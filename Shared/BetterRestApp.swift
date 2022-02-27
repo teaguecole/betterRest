5011792510e560141e8aa84cd19ec2498d74dd3f
Teague Cole <tnc0419@icloud.com>
2022-02-26 22:57:58 -0500

    Initial Commit

---
+++ Shared/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  Shared
+//
+//  Created by Teague Cole on 2/26/22.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
