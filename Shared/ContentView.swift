cfc015a28a39dbe0295bccf41b12ea51a13778e9
Teague Cole <tnc0419@icloud.com>
2022-02-26 23:41:04 -0500

    connected swift ui to core ml, app works

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -5,6 +5,7 @@
 //  Created by Teague Cole on 2/26/22.
 //
 
+import CoreML
 import SwiftUI
 
 struct ContentView: View {
@@ -12,6 +13,10 @@ struct ContentView: View {
     @State private var wakeUp = Date.now
     @State private var sleepAmount = 8.0
     @State private var coffeeAmount = 1
+    
+    @State private var alertTitle = ""
+    @State private var alertMessage = ""
+    @State private var showingAlert = false
     
     var body: some View {
         NavigationView {
@@ -45,11 +50,36 @@ struct ContentView: View {
             .toolbar {
                 Button("Calculate", action: calculateBedTime)
             }
+            .alert(alertTitle,
+                   isPresented: $showingAlert) {
+                Button("OK") {}
+            } message: {
+                Text(alertMessage)
+            }
         }
     }
     
     func calculateBedTime() {
-        
+        do {
+            let config = MLModelConfiguration()
+            let model = try SleepCalculator(configuration: config)
+            let components = Calendar.current.dateComponents([.hour, .minute],
+                                                             from: wakeUp)
+            let hour = (components.hour ?? 0) * 60 * 60
+            let minute = (components.minute ?? 0) * 60
+            
+            let prediction = try model.prediction(wake: Double(hour + minute),
+                                                  estimatedSleep: sleepAmount,
+                                                  coffee: Double(coffeeAmount))
+            let sleepTime = wakeUp - prediction.actualSleep
+            alertTitle = "Your ideal bed time is..."
+            alertMessage = sleepTime.formatted(date: .omitted,
+                                               time: .shortened)
+        } catch {
+            alertTitle = "Error"
+            alertMessage = "Sorry there was a problem calculating your bedtime"
+        }
+        showingAlert = true
     }
 }
 
